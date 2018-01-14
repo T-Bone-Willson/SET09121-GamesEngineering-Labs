cbe605e014ecf83855425deefad3e81141483f2e
T-Bone-Willson <dan.nash.1991@gmail.com>
2018-01-14 15:36:34 +0000

    Left Paddle Collision works, as well as top and bottom wall. Left paddle collision does not work. Will investigate. Left and Right Paddles have their own controls, so both can move independently, i.e. 2 players

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -1,6 +1,6 @@
 #include <SFML/Graphics.hpp>
 
-// Below is main.cpp code, it's commented out because 
+// Below is main.cpp code, it's commented out because... reasons
 /*int main(){
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 	sf::CircleShape shape(100.f);
@@ -42,6 +42,10 @@ const int gameWidth = 800;
 const int gameHeight = 600;
 // Default speed of paddle
 const float paddleSpeed = 400.f;
+// Velolcity for the ball using a Vector2f data type. Speed + Direction.
+Vector2f ballVelocity;
+// decides which way the ball is moving at the start of a game.
+bool server = false;
 
 // Define shape of PONG ball
 CircleShape ball;
@@ -59,11 +63,28 @@ void Load() {
 	// Set size and origin of ball
 	ball.setRadius(ballRadius - 3); // Again, why define radius previously then subtract by 3 here?...
 	ball.setOrigin(ballRadius / 2, ballRadius / 2); // Ask same about the mathemtaical logic here!...
+
+	
+	// I CAN MAYBE DELETE THIS SEGMENT OF COMMENTED CODE!!!
+	/*
 	// reset paddle position
 	paddles[0].setPosition(10 + paddleSize.x / 2, gameHeight / 2);
 	paddles[1].setPosition(770 + paddleSize.x / 2, gameHeight / 2); //Check with Sam if this is correct!!!
 	// reset Ball position
 	ball.setPosition(gameWidth / 2, gameHeight / 2); //May not work...
+	*/
+	
+	// Inline IF Statement, "If the statement befoe the ? is true, then ballVelocity.x is set to -100.0f, 
+	// if server is false ballVelocity.x = 60.0f"
+	ballVelocity = { (server ? 100.0f : -100.0f), 60.0f };
+}
+
+void Reset() {
+	// reset paddle position
+	paddles[0].setPosition(10 + paddleSize.x / 2, gameHeight / 2);
+	paddles[1].setPosition(770 + paddleSize.x / 2, gameHeight / 2); //Check with Sam if this is correct!!!
+	// reset Ball position
+	ball.setPosition(gameWidth / 2, gameHeight / 2); //May not work... IT WORKS!!!
 }
 
 void Update(RenderWindow &window) {
@@ -85,14 +106,79 @@ void Update(RenderWindow &window) {
 	}
 
 	// Handle paddle movement
+	// IF CONTROLING/MOVING BOTH AT THE SAME TIME, RIGHT PADDLE WILL GO FASTER!!!! DUNKT KNOW WHY, ASK SAM!!!
 	float direction = 0.0f;
 	if (Keyboard::isKeyPressed(controls[0])) {
 		direction--; // Paddle goes down vertically
+		// Add relevant paddle (Left) to desired controls
+		paddles[0].move(0, direction * paddleSpeed * dt);
 	}
 	if (Keyboard::isKeyPressed(controls[1])) {
 		direction++; // Paddle goes up vertically
+		paddles[0].move(0, direction * paddleSpeed * dt);
 	}
-	paddles[0].move(0, direction * paddleSpeed * dt);
+	if (Keyboard::isKeyPressed(controls[2])) {
+		direction--; // Paddle goes down vertically
+		// Add relevant paddle (Right) to desired controls
+		paddles[1].move(0, direction * paddleSpeed * dt);
+	}
+	if (Keyboard::isKeyPressed(controls[3])) {
+		direction++; // Paddle goes up vertically
+		paddles[1].move(0, direction * paddleSpeed * dt);
+	}
+
+	// Move ball by it's velocity in correspondence to Delta Time
+	ball.move(ballVelocity * dt);
+
+	// check ball collision
+	const float bx = ball.getPosition().x;
+	const float by = ball.getPosition().y;
+	if (by > gameHeight) {
+		// Bottom Wall
+		ballVelocity.x *= 1.1f;
+		ballVelocity.y *= -1.1f;
+		ball.move(0, -10);
+	}
+	else if (by < 0) {
+		// Top Wall
+		ballVelocity.x *= 1.1f;
+		ballVelocity.y *= -1.1f;
+		ball.move(0, 10);
+	}
+	else if (bx > gameWidth){
+		// Right Wall
+		Reset();
+	}
+	else if (bx < 0) {
+		// Left Wall
+		Reset();
+	}
+	else if (
+		// Ball is inline or behind paddle
+		bx < paddleSize.x 
+		&& // AND ball is below top edge of paddle
+		by > paddles[0].getPosition().y - (paddleSize.y * 0.5)
+		&& // AND ball is above bottom edge of paddle
+		by < paddles[0].getPosition().y + (paddleSize.y * 0.5)
+		) {
+		// Bounce off left paddle
+		// I swaped the -1.1f on the .y to the .x. This means it's now calculutlating a collision/rebound on it's x Axis rather than it's y axis
+		ballVelocity.x *= -1.1f;
+		ballVelocity.y *= 1.1f;
+		ball.move(0, -10);
+	}
+	else if (
+		bx < paddleSize.x
+		&&
+		by > paddles[1].getPosition().y - (paddleSize.y * 0.5)
+		&&
+		by < paddles[1].getPosition().y + (paddleSize.y * 0.5)
+		) {
+		// Bounce off right paddle
+		ballVelocity.x *= -1.1f;
+		ballVelocity.y *= 1.1f;
+		ball.move(0, 10);
+	}
 }
 
 void Render(RenderWindow &window) {
