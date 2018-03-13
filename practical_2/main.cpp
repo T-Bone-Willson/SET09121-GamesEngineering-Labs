161258295c9f687dd5375b0775cc8c60c427c674
T-Bone-Willson <dan.nash.1991@gmail.com>
2018-03-13 15:56:52 +0000

    I believe a rudimentary boilerplate game loop is made for P2. Will begin work for SI

---
+++ practical_2/main.cpp
@@ -0,0 +1,31 @@
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
+
+void Load() {
+	//
+
+}
+
+void Update(double dt) {
+	// Update Everything
+	static sf::Clock clock;
+	//const float dt = clock.restart()asSeconds();
+}
+
+void Render() {
+	// Draw Everything
+
+}
+
+int Main() {
+	// Initialise and Load
+	//while() {
+		// Calculate DT
+		//Update(dt);
+		//Render();
+		// Wait for Vsync
+	//}
+	// Unload and shutdown
+}
